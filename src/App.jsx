02603cc83115dad0d0b31d5ee5936a8808af07a1
derek-pham <derekpham3@gmail.com>
2024-06-11 14:10:02 +1000

    - lesson09

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import AnimationsLesson from './scenes/AnimationsLesson'
 import CamerasLesson from './scenes/CamerasLesson'
 import FullscreenAndResizing from './scenes/FullscreenAndResizing'
 import GeometriesLesson from './scenes/GeometriesLesson'
+import DebugUILesson from './scenes/DebugUILesson'
 
 function App() {
 
   return (
     <>
-    <GeometriesLesson/>
+    <DebugUILesson/>
     </>
   )
 }
--- src/scenes/DebugUILesson.jsx
+++ src/scenes/DebugUILesson.jsx
@@ -1,11 +1,26 @@
 import { useEffect, useRef } from 'react';
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import GUI from 'lil-gui';
+import gsap from 'gsap';
 
-const BasicCubeScene = () => {
+
+const DebugUILesson = () => {
     const canvasRef = useRef();
 
     useEffect(() => {
+        // Instantiate GUI
+        const gui = new GUI({width: 900, title: 'DEBUG UI'})
+
+        gui.hide()
+        window.addEventListener('keydown', (event) => {
+            if (event.key == 'h') {
+                gui.show(gui._hidden)
+            }
+        })
+        const debugObject = {}
+        debugObject.color = '#c863e3'
+
         // Canvas
         const canvas = canvasRef.current;
 
@@ -16,14 +31,40 @@ const BasicCubeScene = () => {
         const geometry = new THREE.BoxGeometry(1, 1, 1)
 
         // Material
-        const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+        const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: false })
 
         //Mesh Object
         const mesh = new THREE.Mesh(geometry, material)
         scene.add(mesh)
 
+        // ADDING THINGS TO GUI
+        const cubeTweaks = gui.addFolder('HELLO')
+        // gui.close()
+
+        cubeTweaks.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('Elevation') // Add object and then object's property
+        cubeTweaks.add(mesh, 'visible').name("Visible")
+        cubeTweaks.add(material, 'wireframe') // mesh.material also works since they ultimately target the same property
+        cubeTweaks.addColor(debugObject, 'color').onChange(() => material.color.set(debugObject.color))
+        debugObject.spin = () => {
+            gsap.to(mesh.rotation, { x: mesh.rotation.x + Math.PI * 2 })
+        }
+        cubeTweaks.add(debugObject, 'spin').name('SPIN')
+
+        debugObject.subDivision = 2
+        cubeTweaks.add(debugObject, 'subDivision').min(1).max(10).step(1)
+            .onFinishChange(() => { // onFinishChange for performance reasons
+                mesh.geometry.dispose() // Destroys current object before creating a new one to prevent memory leaks and performance issues
+                mesh.geometry = new THREE.BoxGeometry(
+                    1, 1, 1,
+                    debugObject.subDivision, debugObject.subDivision, debugObject.subDivision
+                )
+            })
+
+        const myObject = { myVariable: 1335 }
+        cubeTweaks.add(myObject, 'myVariable')
+
         // Camera
-        const sizes = { width: window.innerWidth, height: window.innerHeight}
+        const sizes = { width: window.innerWidth, height: window.innerHeight }
         const camera = new THREE.PerspectiveCamera(110, sizes.width / sizes.height)
         camera.position.set(0, 0, 2)
         scene.add(camera)
@@ -76,4 +117,4 @@ const BasicCubeScene = () => {
     )
 }
 
-export default BasicCubeScene
+export default DebugUILesson
